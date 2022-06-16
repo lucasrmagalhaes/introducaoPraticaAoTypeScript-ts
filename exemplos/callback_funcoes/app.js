3bd9ea4533227638a489b6af1d76dc391e210b98
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-06-16 16:01:09 -0300

    Incluindo callback em funções

---
+++ exemplos/callback_funcoes/app.js
@@ -0,0 +1,16 @@
+"use strict";
+function printaValoresNumericos(numero1, numero2) {
+    console.log(numero1 + numero2);
+}
+function somarValoresNumericosETratar(numero1, numero2, callback) {
+    let resultado = numero1 + numero2;
+    return callback(resultado);
+}
+function aoQuadrado(numero) {
+    return numero * numero;
+}
+function dividirPorEleMesmo(numero) {
+    return numero / numero;
+}
+console.log(somarValoresNumericosETratar(1, 3, aoQuadrado));
+console.log(somarValoresNumericosETratar(1, 3, dividirPorEleMesmo));
---
+++ exemplos/callback_funcoes/app.ts
@@ -0,0 +1,23 @@
+function printaValoresNumericos(numero1: number, numero2: number): void {
+    console.log(numero1 + numero2)    
+}
+
+function somarValoresNumericosETratar(
+    numero1: number, numero2: number,
+    callback:(numero: number) => number
+): number {
+    let resultado = numero1 + numero2;
+
+    return callback(resultado);
+}
+
+function aoQuadrado(numero: number): number {
+    return numero * numero;
+}
+
+function dividirPorEleMesmo(numero: number): number {
+    return numero / numero;
+}
+
+console.log(somarValoresNumericosETratar(1, 3, aoQuadrado));
+console.log(somarValoresNumericosETratar(1, 3, dividirPorEleMesmo));
