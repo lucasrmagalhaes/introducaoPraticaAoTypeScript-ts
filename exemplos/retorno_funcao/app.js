2a7e6585a213ad5ec734584b6e5cd5f30ac15b40
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-06-16 15:53:32 -0300

    Quando usar o tipo void em funções

--- exemplos/retorno_funcao/app.js
+++ exemplos/retorno_funcao/app.js
@@ -3,3 +3,6 @@ function somarValoresNumericos(numero1, numero2) {
     return numero1 + numero2;
 }
 console.log(somarValoresNumericos(1, 3));
+function printaValoresNumericos(numero1, numero2) {
+    console.log(numero1 + numero2);
+}
--- exemplos/retorno_funcao/app.ts
+++ exemplos/retorno_funcao/app.ts
@@ -3,3 +3,7 @@ function somarValoresNumericos(numero1: number, numero2: number): number {
 }
 
 console.log(somarValoresNumericos(1, 3));
+
+function printaValoresNumericos(numero1: number, numero2: number): void {
+    console.log(numero1 + numero2)    
+}
